b27a82344b48a33ccde86cc001c7682e8a764137
Shane Myrick <shane@apollographql.com>
2022-09-29 18:04:01 -0700

    Add basic action code

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# Solutions Repo Template
-
-Give your project a relevant name and description in this README.
+# Apollo GitHub Actions
 
 **The code in this repository is experimental and has been provided for reference purposes only. Community feedback is welcome but this project may not be supported in the same way that repositories in the official [Apollo GraphQL GitHub organization](https://github.com/apollographql) are. If you need help you can file an issue on this repository, [contact Apollo](https://www.apollographql.com/contact-sales) to talk to an expert, or create a ticket directly in Apollo Studio.**
 
---
+++ index.js
@@ -0,0 +1,34 @@
+import * as core from '@actions/core';
+import * as toolCache from '@actions/tool-cache';
+
+async function run() {
+    // Get version to install (defaults to "latest")
+    const version = core.getInput('version');
+    const url = `https://rover.apollo.dev/nix/${version}`;
+
+    // Install the resolved version if necessary
+    const toolPath = toolCache.find('rover', version)
+    if (toolPath) {
+        core.addPath(toolPath)
+    } else {
+        core.info(`Downloading Rover ${version} from ${url}...`)
+        await installRover(url, version)
+    }
+
+    core.info(`Rover ${version} is installed`)
+}
+
+async function installRover(url, resolvedVersion) {
+    const tarPath = await toolCache.downloadTool(url)
+    const extractedPath = await toolCache.extractTar(tarPath)
+    const cachedPath = await toolCache.cacheDir(extractedPath, 'rover', resolvedVersion)
+    core.addPath(cachedPath)
+}
+
+run().catch((error) => {
+    if (error instanceof Error) {
+        core.setFailed(error.message)
+    } else {
+        core.setFailed(`${error}`)
+    }
+})
