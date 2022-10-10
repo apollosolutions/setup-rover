b9eb357b9ffb51ae71f1ec4d1570c4034ed95aaf
Shane Myrick <shane@apollographql.com>
2022-10-10 14:15:54 -0700

    Build code again

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,7 @@ require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 
 const core = __nccwpck_require__(2186);
 const toolCache = __nccwpck_require__(7784);
+const path = __nccwpck_require__(5622);
 
 async function setup() {
     try {
@@ -15,7 +16,7 @@ async function setup() {
         const url = `https://github.com/apollographql/rover/releases/download/${version}/${tarFileName}.tar.gz`;
         core.info(`Downloading Rover from ${url}`);
 
-        const toolPath = await getPath(url, version, arch);
+        const toolPath = await getPath(url, version);
         core.addPath(toolPath);
         core.info(`Rover ${version} is installed at ${toolPath}`);
     } catch (error) {
@@ -27,22 +28,23 @@ async function setup() {
     }
 }
 
-async function getPath(url, version, arch) {
-    const toolPath = toolCache.find('rover', version, arch);
+async function getPath(url, version) {
+    const toolPath = toolCache.find('rover', version);
     if (toolPath) {
         core.info('Rover is already installed on tool path');
         return toolPath;
     } else {
         core.info('Rover not found on tool path. Downloading file');
-        return installRover(url, version, arch);
+        return installRover(url, version);
     }
 }
 
-async function installRover(url, version, arch) {
+async function installRover(url, version) {
     const downloadPath = await toolCache.downloadTool(url);
     const extract = url.endsWith('.zip') ? toolCache.extractZip : toolCache.extractTar;
     const extractedPath = await extract(downloadPath);
-    return toolCache.cacheDir(extractedPath, 'rover', version, arch);
+    const pathToCli = path.join(extractedPath, 'dist');
+    return toolCache.cacheDir(pathToCli, 'rover', version);
 }
 
 module.exports = setup;
