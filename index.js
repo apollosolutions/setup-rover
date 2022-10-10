67cf52a3acc13d8ff00117b11ca661addf7f1080
Shane Myrick <shane@apollographql.com>
2022-10-10 11:56:47 -0700

    Add debug logs

--- index.js
+++ index.js
@@ -5,30 +5,32 @@ async function run() {
     // Get version to install (defaults to "latest")
     const version = core.getInput('version');
     const url = `https://rover.apollo.dev/nix/${version}`;
+    core.info(`Downloading Rover ${version} from ${url}`);
 
     // Install the resolved version if necessary
-    const toolPath = toolCache.find('rover', version)
+    const toolPath = toolCache.find('rover', version);
     if (toolPath) {
-        core.addPath(toolPath)
+        core.info('Rover is already installed on tool path');
+        core.addPath(toolPath);
     } else {
-        core.info(`Downloading Rover ${version} from ${url}...`)
-        await installRover(url, version)
+        core.info('Rover not found on tool path. Downloading file');
+        await installRover(url, version);
     }
 
-    core.info(`Rover ${version} is installed`)
+    core.info(`Rover ${version} is installed`);
 }
 
 async function installRover(url, version) {
     const tarPath = await toolCache.downloadTool(url);
-    const extractedPath = await toolCache.extractTar(tarPath)
-    const cachedPath = await toolCache.cacheDir(extractedPath, 'rover', version)
-    core.addPath(cachedPath)
+    const extractedPath = await toolCache.extractTar(tarPath);
+    const cachedPath = await toolCache.cacheDir(extractedPath, 'rover', version);
+    core.addPath(cachedPath);
 }
 
 run().catch((error) => {
     if (error instanceof Error) {
-        core.setFailed(error.message)
+        core.setFailed(error.message);
     } else {
-        core.setFailed(`${error}`)
+        core.setFailed(`${error}`);
     }
-})
+});
